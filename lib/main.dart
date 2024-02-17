8faa55376fac30dfd745980b032d973c983004db
AbdoAlhady <abdo.agiry90@gmail.com>
2024-02-17 03:20:54 +0200

    - added SharedPreferences - added cache helper

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/app/todo_app.dart';
+import 'package:todo_app/core/database/cache_helper.dart';
 import 'package:todo_app/core/routing/app_routing.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
   runApp(TodoApp(
     appRouter: AppRouter(),
   ));
